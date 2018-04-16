65bfc086763748560609035295d8e14c033536cb
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-04-16 10:48:37 +0200

    Add read queries

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const Book = require('../models/book-model');
+
 const router  = express.Router();
 
 /* GET home page */
@@ -8,6 +11,31 @@ router.get('/', (req, res, next) => {
 
 router.get('/about', (req, res, next) => {
   res.render('about-page');
+});
+
+router.get('/books', (req, res, next) => {
+  Book.find()
+    .then((booksFromDb) => {
+      // send the database results to the view
+      res.locals.bookList = booksFromDb;
+      res.render('books-page');
+    })
+    .catch((err) => {
+      // show the error page with this error
+      next(err);
+    });
+});
+
+router.get('/book/:bookId', (req, res, next) => {
+  Book.findById(req.params.bookId)
+    .then((bookDetails) => {
+      res.locals.book = bookDetails;
+      res.render('single-book-page');
+    })
+    .catch((err) => {
+      // show the error page with this error
+      next(err);
+    });
 });
 
 module.exports = router;
