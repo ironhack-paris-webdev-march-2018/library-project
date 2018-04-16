34581ce5318aff073903c78f0e0f3c87ff21c867
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-04-16 16:54:17 +0200

    Complete CRUD

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,59 @@ router.get('/books', (req, res, next) => {
     });
 });
 
+// STEP #1 of CREATE form (show the form)
+router.get('/book/add', (req, res, next) => {
+  res.render('book-form');
+});
+
+// STEP #5 of CREATE form (server processes)
+router.post('/process-book', (req, res, next) => {
+  const { title, author, description, rating } = req.body;
+
+  Book.create({ title, author, description, rating })
+    .then(() => {
+      // redirect ONLY to URLs
+      // (redirect so we don't get duplicate data on refresh)
+
+      // STEP #6 of CREATE form (server responds)
+      res.redirect('/books');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// STEP #1 of UPDATE form (show the form)
+router.get('/book/:bookId/edit', (req, res, next) => {
+  Book.findById(req.params.bookId)
+    .then((bookDetails) => {
+      res.locals.bookId = req.params.bookId;
+      res.locals.book = bookDetails;
+      res.render('book-edit');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// STEP #5 of UPDATE form (server processes)
+router.post('/process-edit/:bookId', (req, res, next) => {
+  const { title, author, description, rating } = req.body;
+
+  Book.findByIdAndUpdate(
+    req.params.bookId,                      // which document to update
+    { title, author, description, rating }, // what changes to make
+    { runValidators: true }                 // extra settings
+  )
+  .then(() => {
+    // STEP #6 of UPDATE form (server responds)
+    res.redirect(`/book/${req.params.bookId}`);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
 router.get('/book/:bookId', (req, res, next) => {
   Book.findById(req.params.bookId)
     .then((bookDetails) => {
@@ -34,6 +87,16 @@ router.get('/book/:bookId', (req, res, next) => {
     })
     .catch((err) => {
       // show the error page with this error
+      next(err);
+    });
+});
+
+router.get('/book/:bookId/delete', (req, res, next) => {
+  Book.findByIdAndRemove(req.params.bookId)
+    .then(() => {
+      res.redirect('/books');
+    })
+    .catch((err) => {
       next(err);
     });
 });
